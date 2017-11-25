fdf9a331c88ee5c9639f3db8d5e0175a4644b1fc
= <=>
2017-11-25 23:12:24 +0000

    pics

--- motion-detection.py
+++ motion-detection.py
@@ -4,6 +4,11 @@ import os
 import time
 from datetime import datetime
 from PIL import Image
+from picamera import PiCamera
+from io import BytesIO
+from time import sleep
+from picamera import PiCamera
+from PIL import Image
 
 # Motion detection settings:
 # Threshold (how much a pixel has to change by to be marked as "changed")
@@ -11,24 +16,28 @@ from PIL import Image
 # ForceCapture (whether to force an image to be captured every forceCaptureTime seconds)
 threshold = 10
 sensitivity = 20
-forceCapture = True
+forceCapture = False
 forceCaptureTime = 5 # Every five seconds
 
 # File settings
 saveWidth = 1280
 saveHeight = 960
 diskSpaceToReserve = 40 * 1024 * 1024 # Keep 40 mb free on disk
+camera = PiCamera()
+
 
 # Capture a small test image (for motion detection)
 def captureTestImage():
-    print("capturing test image")
-    command = "raspistill -w %s -h %s -t 0 -e bmp -o -" % (100, 75)
-    imageData = StringIO.StringIO()
-    imageData.write(subprocess.check_output(command, shell=True))
-    imageData.seek(0)
-    im = Image.open(imageData)
+    # Create the in-memory stream
+    stream = BytesIO()
+    camera.start_preview()
+    sleep(2)
+    camera.capture(stream, format='jpeg')
+    stream.seek(0)
+    im = Image.open(stream)
+    print("4")
     buffer = im.load()
-    imageData.close()
+    print("5")
     return im, buffer
 
 # Save a full size image to disk
@@ -37,7 +46,7 @@ def saveImage(width, height, diskSpaceToReserve):
     keepDiskSpaceFree(diskSpaceToReserve)
     time = datetime.now()
     filename = "capture-%04d%02d%02d-%02d%02d%02d.jpg" % (time.year, time.month, time.day, time.hour, time.minute, time.second)
-    subprocess.call("raspistill -w 1296 -h 972 -t 0 -e jpg -q 15 -o %s" % filename, shell=True)
+    camera.capture(filename)
     print "Captured %s" % filename
 
 # Keep free space above given level
@@ -57,13 +66,12 @@ def getFreeSpace():
     return du
         
 # Get first image
-image1 = buffer1 = None
 
 # Reset last capture time
-lastCapture = time.time()
-
 def main():
     print("IN MAIN")
+    lastCapture = time.time()
+    image1, buffer1 = captureTestImage()
     while True:
 
         print("running loop")
@@ -75,10 +83,11 @@ def main():
     	changedPixels = 0
     	for x in xrange(0, 100):
        	    for y in xrange(0, 75):
-	    	print("checking for motion")
+                print("getting pixel diff")
             	# Just check green channel as it's the highest quality channel
             	pixdiff = abs(buffer1[x,y][1] - buffer2[x,y][1])
             	if pixdiff > threshold:
+                    print("motion detected")
                     changedPixels += 1
 
     	# Check force capture
